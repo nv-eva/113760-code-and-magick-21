1ada5c73cfe07c3f9458b58328fbc1528e16ec51
nv-eva <ds222591@rambler.ru>
2020-10-04 13:06:54 +0300

    Отрисовывает график

--- js/stat.js
+++ js/stat.js
@@ -6,45 +6,72 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP_SHADOW = 10;
 var GAP_TEXT = 20;
+var GAP_BAR = 50;
+var BAR_WIDTH = 40;
+var BAR_HEIGHT = 150;
+var barHeight = BAR_HEIGHT;
 
-var textTitle = ['Ура вы победили!', 'Список результатов: '];
+var textTitle = [`Ура вы победили!`, `Список результатов: `];
 
-var renderCloud = function(ctx, x, y, fillColor, strokeColor) {
+var renderCloud = function (ctx, x, y, fillColor, strokeColor) {
   ctx.fillStyle = fillColor;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
   ctx.strokeStyle = strokeColor;
   ctx.strokeRect(x - 1, y - 1, CLOUD_WIDTH + 1, CLOUD_HEIGHT + 1);
 };
 
-var renderText = function(ctx, text, x, y) {
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
+var renderText = function (ctx, text, x, y) {
+  ctx.fillStyle = `#000`;
+  ctx.font = `16px PT Mono`;
+  ctx.textBaseline = `hanging`;
   ctx.fillText(text, x, y);
 };
 
-window.renderStatistics = function (ctx) {
+window.renderStatistics = function (ctx, players) {
   renderCloud(
-    ctx,
-    CLOUD_X + GAP_SHADOW,
-    CLOUD_Y + GAP_SHADOW,
-    'rgba(0, 0, 0, 0.7)',
-    'rgba(0, 0, 0, 0)'
+      ctx,
+      CLOUD_X + GAP_SHADOW,
+      CLOUD_Y + GAP_SHADOW,
+      `rgba(0, 0, 0, 0.7)`,
+      `rgba(0, 0, 0, 0)`
   );
   renderCloud(
-    ctx,
-    CLOUD_X,
-    CLOUD_Y,
-    '#fff',
-    'rgba(0, 0, 0, 0.7)'
+      ctx,
+      CLOUD_X,
+      CLOUD_Y,
+      `#fff`,
+      `rgba(0, 0, 0, 0.7)`
   );
 
   for (var i = 0; i < textTitle.length; i++) {
     renderText(
-      ctx,
-      textTitle[i],
-      CLOUD_X + GAP_TEXT,
-      CLOUD_Y + GAP_TEXT * (i + 1)
+        ctx,
+        textTitle[i],
+        CLOUD_X + GAP_TEXT,
+        CLOUD_Y + GAP_TEXT * (i + 1)
+    );
+  }
+
+  for (var j = 0; j < players.length; j++) {
+    renderText(
+        ctx,
+        players[j],
+        CLOUD_X + GAP_BAR + (GAP_BAR + BAR_WIDTH) * j,
+        CLOUD_Y + GAP_TEXT * (textTitle.length + 1) + 5
+    );
+
+    ctx.fillRect(
+        CLOUD_X + GAP_BAR + (GAP_BAR + BAR_WIDTH) * j,
+        CLOUD_Y + GAP_TEXT * (textTitle.length + 1) + 5 + GAP_TEXT,
+        BAR_WIDTH,
+        barHeight
+    );
+
+    renderText(
+        ctx,
+        players[j],
+        CLOUD_X + GAP_BAR + (GAP_BAR + BAR_WIDTH) * j,
+        CLOUD_Y + GAP_TEXT * (textTitle.length + 1) + 15 + GAP_TEXT + BAR_HEIGHT
     );
   }
 };
