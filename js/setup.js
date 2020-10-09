ee46a8fe3147e9d3434eaf8cbd4645b1b673af7f
nv-eva <ds222591@rambler.ru>
2020-10-09 17:07:46 +0300

    Генерирует массив волшебников

--- js/setup.js
+++ js/setup.js
@@ -1,11 +1,9 @@
 'use strict';
 
 const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
-/*
 const WIZARD_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const WIZARD_COATS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const WIZARD_EYES = [`black`, `red`, `blue`, `yellow`, `green`];
-*/
 
 // 1. Показывает блок setup
 const userDialog = document.querySelector(`.setup`);
@@ -35,7 +33,17 @@ const wizards = [
   }
 ];
 
-// 3. Создает DOM-элементы волшбников на основешаблона
+const getRandomIndex = function (array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+for (let j = 0; j < 4; j++) {
+  wizards[j].name = getRandomIndex(WIZARD_NAMES) + ' ' + getRandomIndex(WIZARD_SURNAMES);
+  wizards[j].coatColor = getRandomIndex(WIZARD_COATS);
+  wizards[j].eyes = getRandomIndex(WIZARD_EYES);
+};
+
+// 3. Создает DOM-элементы волшбников на основе шаблона
 const similarListElement = userDialog.querySelector('.setup-similar-list');
 
 const similarWizardTemplate = document.querySelector('#similar-wizard-template')
