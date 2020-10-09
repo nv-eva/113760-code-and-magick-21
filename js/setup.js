19dc76f4ce2c91ee730457a1691e5af6a6e8407d
nv-eva <ds222591@rambler.ru>
2020-10-09 15:05:44 +0300

    Выводит волшебников на страницу

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,62 @@
 'use strict';
 
+const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
+/*
+const WIZARD_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
+const WIZARD_COATS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
+const WIZARD_EYES = [`black`, `red`, `blue`, `yellow`, `green`];
+*/
+
+// 1. Показывает блок setup
 const userDialog = document.querySelector(`.setup`);
 userDialog.classList.remove(`hidden`);
+
+// 2. Генерирует массив волшебников
+const wizards = [
+  {
+    name: WIZARD_NAMES[0],
+    coatColor: 'rgb(241, 43, 107)',
+    eyes: 'black'
+  },
+  {
+    name: WIZARD_NAMES[1],
+    coatColor: 'rgb(215, 210, 55)',
+    eyes: 'green'
+  },
+  {
+    name: WIZARD_NAMES[2],
+    coatColor: 'rgb(101, 137, 164)',
+    eyes: 'blue'
+  },
+  {
+    name: WIZARD_NAMES[3],
+    coatColor: 'rgb(127, 127, 127)',
+    eyes: 'gray'
+  }
+];
+
+// 3. Создает DOM-элементы волшбников на основешаблона
+const similarListElement = userDialog.querySelector('.setup-similar-list');
+
+const similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+
+const renderWizard = function (wizard) {
+  const wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyes;
+  return wizardElement;
+};
+
+// 4. Вставляет сгенерированные элементы на страницу
+const fragment = document.createDocumentFragment();
+for (let i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+// 5. Показывает блок setup-similar
+const chooseWizards = userDialog.querySelector(`.setup-similar`);
+chooseWizards.classList.remove(`hidden`);
