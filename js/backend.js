ee6fec56ad31c8d2a8e0b3c3188f72eed5425387
nv-eva <ds222591@rambler.ru>
2020-10-30 20:36:28 +0300

    Добавляет backend.load

--- js/backend.js
+++ js/backend.js
@@ -1,8 +1,22 @@
 'use strict';
 (function () {
-  const URL = `https://21.javascript.pages.academy/code-and-magick`;
+  const URL_LOAD = `https://21.javascript.pages.academy/code-and-magick/data`;
+  const URL_SAVE = `https://21.javascript.pages.academy/code-and-magick`;
 
   window.backend = {
+    load(onLoad, onError) {
+      const xhr = new XMLHttpRequest();
+      xhr.responseType = `json`;
+
+      xhr.open(`GET`, URL_LOAD);
+
+      xhr.addEventListener(`load`, function () {
+        onLoad(xhr.response);
+      });
+
+      xhr.send();
+    },
+
     save(data, onLoad) {
       const xhr = new XMLHttpRequest();
       xhr.responseType = `json`;
@@ -11,7 +25,7 @@
         onLoad(xhr.response);
       });
 
-      xhr.open(`POST`, URL);
+      xhr.open(`POST`, URL_SAVE);
       xhr.send(data);
     }
   };
--- js/setup.js
+++ js/setup.js
@@ -1,14 +1,16 @@
 'use strict';
 
 (function () {
-  const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
-  const WIZARD_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
   const WIZARD_COATS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
   const WIZARD_EYES = [`black`, `red`, `blue`, `yellow`, `green`];
   const FIREBALL = [`#ee4830`, `#30a8ee`, `#5ce6c0`, `#e848d5`, `#e6e848`];
   const COUNT_WIZARDS = 4;
 
+  /*
   // Генерирует массив волшебников
+  const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
+  const WIZARD_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
+
   const generateWizard = function () {
     const wizard = {};
 
@@ -28,6 +30,7 @@
     (new Array(countWizards)).fill(``).map(generateWizard);
 
   const wizards = generateWizards(COUNT_WIZARDS);
+  */
 
   // Создает DOM-элементы волшбников на основе шаблона
   const similarListElement = window.setup.querySelector(`.setup-similar-list`);
@@ -37,22 +40,31 @@
 
   const renderWizard = function (wizard) {
     const wizardElement = similarWizardTemplate.cloneNode(true);
+
     wizardElement.querySelector(`.setup-similar-label`).textContent = wizard.name;
-    wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
-    wizardElement.querySelector(`.wizard-eyes`).style.fill = wizard.eyes;
+    wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.colorCoat; // coatColor
+    wizardElement.querySelector(`.wizard-eyes`).style.fill = wizard.colorEyes; // eyes
+
     return wizardElement;
   };
 
   // Вставляет сгенерированные элементы на страницу
-  const fragment = document.createDocumentFragment();
-  for (let i = 0; i < wizards.length; i++) {
-    fragment.appendChild(renderWizard(wizards[i]));
-  }
-  similarListElement.appendChild(fragment);
+  window.backend.load(function (wizards) {
+    const fragment = document.createDocumentFragment();
 
-  // Показывает блок setup-similar
-  const chooseWizards = window.setup.querySelector(`.setup-similar`);
-  chooseWizards.classList.remove(`hidden`);
+    let countWizards = COUNT_WIZARDS;
+    if (wizards.length < COUNT_WIZARDS) {
+      countWizards = wizards.length;
+    }
+
+    for (let i = 0; i < countWizards; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+
+    similarListElement.appendChild(fragment);
+
+    window.setup.querySelector(`.setup-similar`).classList.remove(`hidden`);
+  }, function () {});
 
   // Изменяет цвета волшебника по нажатию
   let wizardCoat = window.setupPlayer.querySelector(`.setup-wizard .wizard-coat`);
